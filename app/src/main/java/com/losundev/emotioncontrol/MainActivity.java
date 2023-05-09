086710616b731cc9cbc37832a83fbd93f6b7886a
Kirill <danflatch@gmail.com>
2023-05-09 17:49:16 +0200

    registration

--- app/src/main/java/com/losundev/emotioncontrol/MainActivity.java
+++ app/src/main/java/com/losundev/emotioncontrol/MainActivity.java
@@ -2,7 +2,17 @@ package com.losundev.emotioncontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View v) {
+        EditText password = (EditText) findViewById(R.id.password_Textfield);
+        EditText email = (EditText) findViewById(R.id.email_Textfield);
+        String email_fu = email.getText().toString();
+        String password_fu = password.getText().toString();
+
+        String fileName = "logdata.txt";
+        String line = null;
+        ArrayList<String> wordsList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(getFilesDir(), fileName)))) {
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    wordsList.add(word);
+                }
+            }
+            if (wordsList.isEmpty()) {
+                // File is empty
+                writeStringToFile(this, fileName, email_fu + "" + password_fu);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeStringToFile(Context context, String fileName, String stringToWrite) {
+        try {
+            File file = new File(context.getFilesDir(), fileName);
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(stringToWrite);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
